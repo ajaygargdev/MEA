a745ad99818de92f7a6d3ef3d79327ac95c0c6f5
Ashish Verma <QZG@kmd.dk>
2020-02-26 11:45:42 +0530

    adding swagger generation code in the pipeline

--- src/Kmd.Momentum.Mea.Api/Startup.cs
+++ src/Kmd.Momentum.Mea.Api/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
@@ -6,7 +7,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Kmd.Momentum.Mea.Api
 {
@@ -38,6 +41,35 @@ namespace Kmd.Momentum.Mea.Api
             {
                 options.Predicate = (check) => check.Tags.Contains("ready");
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "KMD Logic STS Bridge",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                });
+
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter 'Bearer' followed by space and a JWT from logic",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securityScheme);
+                var securityRequirement = new OpenApiSecurityRequirement();
+                securityRequirement.Add(securityScheme, new[] { "Bearer" });
+                c.AddSecurityRequirement(securityRequirement);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -51,6 +83,20 @@ namespace Kmd.Momentum.Mea.Api
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
+            app.UseStaticFiles(); // Use with c.InjectStylesheet("/swagger-ui/custom.css");
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Momentum External Api");
+                //    c.SwaggerEndpoint($"/swagger/case/swagger.json", $"KMD Case Service API");
+                //    c.InjectStylesheet("https://fonts.googleapis.com/css?family=Roboto");
+                //    c.InjectStylesheet("/swagger-ui/custom.css");
+                c.DefaultModelRendering(ModelRendering.Model);
+                c.DisplayRequestDuration();
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
 
             app.UseEndpoints(endpoints =>
             {
